52db24c7a8a83a81529c224f5876c45eef29ce9d
pankaj kumar <pankajkumar130697@gmail.com>
2025-09-27 23:32:42 +0530

    first commit

--- src/main/java/com/example/demo/dto/ErrorResponse.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.dto;
-
-public class ErrorResponse {
-    String errorCode;
-    String errorMessage;
-}
---
+++ src/main/java/com/example/demo/service/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,2 @@
+package com.example.demo.service;public class OAuth2AuthenticationSuccessHandler {
+}
